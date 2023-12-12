52ac85db1c7c8751cf7da0a7f9227ece4dba6b09
Noa Aarts <itepastra@gmail.com>
2023-12-12 22:23:24 +0100

    day 12 part 1

--- src/day11.rs
+++ src/day11.rs
@@ -1,3 +1,135 @@
-pub fn day() {
+use crate::utils::read_lines;
+use std::str::FromStr;
 
+pub fn day() {
+    dayp1();
+    dayp2();
+}
+
+fn dayp1() {
+    let mut observed_galaxies = Vec::new();
+    let mut expandable_cols = [true].repeat(256);
+    let mut expandable_rows = Vec::new();
+    if let Ok(lines) = read_lines("data/day11.txt") {
+        for (i, line) in lines.enumerate() {
+            if let Ok(ip) = line {
+                expandable_rows.push(ip.chars().all(|p| p == '.'));
+                expandable_cols = ip
+                    .chars()
+                    .zip(expandable_cols.iter())
+                    .map(|(f, i)| *i && f == '.')
+                    .collect();
+
+                let _: Vec<_> = ip
+                    .chars()
+                    .enumerate()
+                    .map(|(j, f)| match f {
+                        '#' => observed_galaxies.push((i, j)),
+                        _ => {}
+                    })
+                    .collect();
+            }
+        }
+    }
+    let col_offset: Vec<_> = expandable_cols
+        .iter()
+        .scan(0, |acc, cur| {
+            if *cur {
+                *acc += 1;
+            }
+            return Some(*acc);
+        })
+        .collect();
+    let row_offset: Vec<_> = expandable_rows
+        .iter()
+        .scan(0, |acc, cur| {
+            if *cur {
+                *acc += 1;
+            }
+            return Some(*acc);
+        })
+        .collect();
+    let expanded_galaxies: Vec<(usize, usize)> = observed_galaxies
+        .into_iter()
+        .map(|(x, y)| (x + row_offset[x], y + col_offset[y]))
+        .collect();
+    let og_slice = expanded_galaxies.as_slice();
+    let mut total = 0;
+    for i in 0..og_slice.len() {
+        for j in i + 1..og_slice.len() {
+            let dist = galaxy_distance(og_slice[i], og_slice[j]);
+            total += dist;
+        }
+    }
+    println!("day 11 part 1: {}", total);
+}
+
+fn dayp2() {
+    let mut observed_galaxies = Vec::new();
+    let mut expandable_cols = [true].repeat(256);
+    let mut expandable_rows = Vec::new();
+    if let Ok(lines) = read_lines("data/day11.txt") {
+        for (i, line) in lines.enumerate() {
+            if let Ok(ip) = line {
+                expandable_rows.push(ip.chars().all(|p| p == '.'));
+                expandable_cols = ip
+                    .chars()
+                    .zip(expandable_cols.iter())
+                    .map(|(f, i)| *i && f == '.')
+                    .collect();
+
+                let _: Vec<_> = ip
+                    .chars()
+                    .enumerate()
+                    .map(|(j, f)| match f {
+                        '#' => observed_galaxies.push((i, j)),
+                        _ => {}
+                    })
+                    .collect();
+            }
+        }
+    }
+    let col_offset: Vec<_> = expandable_cols
+        .into_iter()
+        .scan(0, |acc, cur| {
+            if cur {
+                *acc += 1000000-1;
+            }
+            return Some(*acc);
+        })
+        .collect();
+    let row_offset: Vec<_> = expandable_rows
+        .into_iter()
+        .scan(0, |acc, cur| {
+            if cur {
+                *acc += 1000000-1;
+            }
+            return Some(*acc);
+        })
+        .collect();
+    let expanded_galaxies: Vec<(usize, usize)> = observed_galaxies
+        .into_iter()
+        .map(|(x, y)| (x + row_offset[x], y + col_offset[y]))
+        .collect();
+    let og_slice = expanded_galaxies.as_slice();
+    let mut total = 0;
+    for i in 0..og_slice.len() {
+        for j in i + 1..og_slice.len() {
+            let dist = galaxy_distance(og_slice[i], og_slice[j]);
+            total += dist;
+        }
+    }
+    println!("day 11 part 2: {}", total);
+}
+
+fn galaxy_distance((ax, ay): (usize, usize), (bx, by): (usize, usize)) -> usize {
+    return abs_diff(ax, bx) + abs_diff(ay, by);
+}
+
+fn abs_diff(slf: usize, other: usize) -> usize {
+    if slf < other {
+        other - slf
+    } else {
+        slf - other
+    }
 }
--- src/day12.rs
+++ src/day12.rs
@@ -1,3 +1,154 @@
-pub fn day() {
+use crate::utils::read_lines;
 
+pub fn day() {
+    dayp1();
 }
+
+fn dayp1() {
+    let mut total = 0;
+    if let Ok(lines) = read_lines("data/day12.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+                if let Some((map, streaks)) = ip.split_once(" ") {
+                    let line_springs = map
+                        .chars()
+                        .map(|c| match c {
+                            '#' => Spring::Broken,
+                            '.' => Spring::Working,
+                            '?' => Spring::Unknown,
+                            _ => todo!(),
+                        })
+                        .collect();
+                    let line_amounts = streaks.split(",").map(|a| a.parse().unwrap()).collect();
+
+                    println!("Trying {:?}, {:?} with total {}", map, line_amounts, total);
+                    total += get_options(&line_springs, 0, &line_amounts, 0, 0, Spring::Working);
+                }
+            }
+        }
+    }
+    println!("day 12 part 1: {}", total);
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Spring {
+    Working,
+    Broken,
+    Unknown,
+}
+
+fn get_options(
+    line: &Vec<Spring>,
+    pos: usize,
+    broken_streaks: &Vec<usize>,
+    broken_streak: usize,
+    broken_parts_since_last: usize,
+    prev: Spring,
+) -> usize {
+    println!("{:?}, at pos {}, at streak {},with n in streak {}", line, pos,  broken_streak, broken_parts_since_last);
+    if broken_streak >= broken_streaks.len() && broken_parts_since_last != 0 {
+        println!("too many streaks... F {:?}", line);
+        return 0;
+    }
+    
+    if pos == line.len() {
+        if broken_streak < broken_streaks.len() - 1 {
+            println!("Not all streaks were done");
+            return 0;
+        } else if broken_streak == broken_streaks.len() && broken_parts_since_last == 0 {
+            println!("still allowed, since it's cleann {:?}", line);
+            return 1;
+        }
+         else if broken_streak >= broken_streaks.len() {
+            println!("too many streaks... F {:?}", line);
+            return 0;
+        }
+        else if broken_streaks[broken_streak] != broken_parts_since_last {
+            println!("Something was not allowed about {:?}, {:?}", line, broken_streaks);
+            return 0;
+        } else {
+            println!("I think this is allowed {:?}", line);
+            return 1;
+        }
+    }
+
+    let opts;
+    match (line[pos], prev) {
+        (Spring::Working, Spring::Working) => {
+            opts = get_options(
+                line,
+                pos + 1,
+                broken_streaks,
+                broken_streak,
+                0,
+                Spring::Working,
+            );
+        }
+        (Spring::Working, Spring::Broken) => {
+            if broken_parts_since_last != broken_streaks[broken_streak] {
+                println!("{:?}'s streak {} was incorrect", line, broken_streak);
+                return 0;
+            }
+            opts = get_options(
+                line,
+                pos + 1,
+                broken_streaks,
+                broken_streak + 1,
+                0,
+                Spring::Working,
+            );
+        }
+        (Spring::Broken, Spring::Broken) => {
+            opts = get_options(
+                line,
+                pos + 1,
+                broken_streaks,
+                broken_streak,
+                broken_parts_since_last + 1,
+                Spring::Broken,
+            );
+        }
+        (Spring::Broken, Spring::Working) => {
+            opts = get_options(
+                line,
+                pos + 1,
+                broken_streaks,
+                broken_streak,
+                1, // is the first one since it started
+                Spring::Broken,
+            );
+        }
+        (Spring::Unknown, Spring::Broken) => {
+            let mut l1 = line.clone();
+            l1[pos] = Spring::Broken;
+            let mut l2 = line.clone();
+            l2[pos] = Spring::Working;
+            if  broken_streak < broken_streaks.len() && broken_parts_since_last != broken_streaks[broken_streak] {
+                println!("{:?} was incorrect, trying only {:?}", l2, l1);
+                return get_options(&l1, pos + 1, broken_streaks, broken_streak, broken_parts_since_last + 1, Spring::Broken);
+            }
+            opts = 
+                // unknown == broken
+                get_options(&l1, pos + 1, broken_streaks, broken_streak, broken_parts_since_last + 1, Spring::Broken) + 
+                // unknown == working
+                get_options(&l2, pos + 1, broken_streaks, broken_streak + 1, 0, Spring::Working);
+        }
+        (Spring::Unknown, Spring::Working) => {
+            let mut l1 = line.clone();
+            l1[pos] = Spring::Broken;
+            let mut l2 = line.clone();
+            l2[pos] = Spring::Working;
+            opts = 
+                // unknown == broken
+                get_options(&l1, pos + 1, broken_streaks, broken_streak, 1, Spring::Broken) + 
+                // unknown == working
+                get_options(&l2, pos + 1, broken_streaks, broken_streak, 0, Spring::Working);
+        }
+        _ => {
+            todo!()
+        }
+    }
+
+    return opts;
+}
+
